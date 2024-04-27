c54637761c0e8b970b85bba4db308c05b582287d
Siew Kam Onn <kosiew@gmail.com>
2024-04-27 18:36:31 +0800

    ignore .env, add .env for NODE_ENV, PORT, MONGO_URI

--- graphql-project-mgmt-main/server/server/config/db.js
+++ graphql-project-mgmt-main/server/server/config/db.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const uri = process.env.MONGO_URI;
+console.log(`mongo_uri: ${uri}`);
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
 
 module.exports = connectDB;
