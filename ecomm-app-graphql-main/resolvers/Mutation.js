963ea2e01c0a446b7e555755aa7b566aef5ee3ac
Siew Kam Onn <kosiew@gmail.com>
2024-04-27 16:36:25 +0800

    Destructure db in Mutation

--- ecomm-app-graphql-main/resolvers/Mutation.js
+++ ecomm-app-graphql-main/resolvers/Mutation.js
@@ -1,14 +1,14 @@
 const { v4: uuid } = require("uuid");
 
 exports.Mutation = {
-  addGenre: (parent, args, { db }) => {
+  addGenre: (parent, args, { db: { genres } }) => {
     const { input } = args;
     const { name } = input;
     const newGene = { id: uuid(), name };
-    db.genres.push(newGene);
+    genres.push(newGene);
     return newGene;
   },
-  addCourse: (parent, args, { db }) => {
+  addCourse: (parent, args, { db: { courses } }) => {
     const { input } = args;
     const { name, description, price, discount, genreId } = input;
     const newCourse = {
@@ -19,10 +19,10 @@ exports.Mutation = {
       discount,
       genreId
     };
-    db.courses.push(newCourse);
+    courses.push(newCourse);
     return newCourse;
   },
-  addReview: (parent, args, { db: reviews }) => {
+  addReview: (parent, args, { db: { reviews } }) => {
     const { input } = args;
     const { date, title, comment, rating, courseId } = input;
     const newReview = { id: uuid(), date, title, comment, rating, courseId };
@@ -40,31 +40,31 @@ exports.Mutation = {
     });
     return true;
   },
-  deleteCourse: (parent, { id }, { db }) => {
-    db.courses = db.courses.filter((course) => course.id !== id);
-    db.reviews = db.reviews.filter((review) => review.courseId !== id);
+  deleteCourse: (parent, { id }, { db: { courses, reviews } }) => {
+    courses = courses.filter((course) => course.id !== id);
+    reviews = reviews.filter((review) => review.courseId !== id);
     return true;
   },
-  deleteReview: (parent, { id }, { db }) => {
-    db.reviews = db.reviews.filter((review) => review.id !== id);
+  deleteReview: (parent, { id }, { db: { reviews } }) => {
+    reviews = reviews.filter((review) => review.id !== id);
     return true;
   },
-  updateGenre: (parent, { id, input }, { db }) => {
-    const index = db.genres.findIndex((genre) => genre.id === id);
+  updateGenre: (parent, { id, input }, { db: { genres } }) => {
+    const index = genres.findIndex((genre) => genre.id === id);
     if (index === -1) return null;
-    db.genres[index] = { ...db.genres[index], ...input };
-    return db.genres[index];
+    genres[index] = { ...genres[index], ...input };
+    return genres[index];
   },
-  updateCourse: (parent, { id, input }, { db }) => {
-    const index = db.courses.findIndex((course) => course.id === id);
+  updateCourse: (parent, { id, input }, { db: { courses } }) => {
+    const index = courses.findIndex((course) => course.id === id);
     if (index === -1) return null;
-    db.courses[index] = { ...db.courses[index], ...input };
-    return db.courses[index];
+    courses[index] = { ...courses[index], ...input };
+    return courses[index];
   },
-  updateReview: (parent, { id, input }, { db }) => {
-    const index = db.reviews.findIndex((review) => review.id === id);
+  updateReview: (parent, { id, input }, { db: { reviews } }) => {
+    const index = reviews.findIndex((review) => review.id === id);
     if (index === -1) return null;
-    db.reviews[index] = { ...db.reviews[index], ...input };
-    return db.reviews[index];
+    reviews[index] = { ...reviews[index], ...input };
+    return reviews[index];
   }
 };
