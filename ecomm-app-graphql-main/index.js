0d75d7b566ba064ea5591436cbac266a877d99e2
Siew Kam Onn <kosiew@gmail.com>
2024-04-27 16:06:04 +0800

    Add comment to explain relationship between db, typeDefs, resolvers

--- ecomm-app-graphql-main/index.js
+++ ecomm-app-graphql-main/index.js
@@ -6,10 +6,31 @@ const { Course } = require("./resolvers/Course");
 const { Genre } = require("./resolvers/Genre");
 const { db } = require("./database");
 
-const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers: { Query, Mutation, Course, Genre }, 
-    context: { db } 
-})
+// relationship between db, typeDefs, resolvers
+// db contains objects with all the fields
+// eg db.courses = [{id: 1, name: "Course 1", description: "Description 1", price: 100, discount: true, genreId: 1}]
+// typeDefs contains the schema of the objects
+//   including Course
+// .     Course {
+//          id: ID!
+//          ...
+//          genre: Genre
+//        }
+//    this describes the Course object and its fields in Query Explorer ie in Explore, we won't see genreId field but genre field
+//    genre field is resolved by resolvers.Course
+//  resolvers.Course contains the logic to resolve the Course object
+//    including the genre field
+//    Course: {
+//      genre: (parent, args, context) => {
+//        const db = context.db;
+//        const genreId = parent.genreId;
+//        return db.genres.find(item => item.id === genreId);
+//      }
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: { Query, Mutation, Course, Genre },
+  context: { db }
+});
 
 server.listen().then(({ url }) => console.log(`Server is running at ${url}`));
